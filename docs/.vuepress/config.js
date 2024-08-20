14ca2e530001945ac12fae0f6bf0dee9bcad781d
xgp <xiegongping2021@163.com>
2024-08-20 09:37:53 +0800

    111

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -5,8 +5,8 @@ import { viteBundler } from "@vuepress/bundler-vite";
 export default defineUserConfig({
   lang: "zh-CN",
 
-  title: "ASP.NET知识库",
-  description: "ASP.NET开发人员必备技能知识库",
+  title: "知识库",
+  description: ".net开发人员必备技能知识库",
 
   theme: defaultTheme({
     logo: "https://vuejs.press/images/hero.png",
